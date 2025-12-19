14b60f1d18e644b88e486cbfb47e15d5082485fb
mojyack <mojyack@gmail.com>
2025-12-19 17:21:18 +0900

    config: define USB_ENABLE_IAP
    Change-Id: I1696dbc8a2304fc5eecc5432b4c52e25801c468e

--- firmware/export/config.h
+++ firmware/export/config.h
@@ -1398,6 +1398,10 @@ Lyre prototype 1 */
 #define USB_ENABLE_AUDIO
 #endif
 
+#if defined(USB_HAS_INTERRUPT) && defined(USB_HAS_ISOCHRONOUS) && USB_BATCH_SLOTS > 0
+#define USB_ENABLE_IAP
+#endif
+
 #endif /* BOOTLOADER */
 
 #endif /* HAVE_USBSTACK */
