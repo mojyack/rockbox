4c8deacde85163d85fdea3372a95fd85388551a6
mojyack <mojyack@gmail.com>
2025-12-19 22:45:47 +0900

    usb: enable cpu boost for specific class drivers
    add needs_cpu_boost field to usb_class_driver and manage boost state in
    usb_core, similar to needs_exclusive_storage.
    
    Change-Id: Ieb0cd7bedda5b24bb0d209d5ce907db30f4815db

--- firmware/usbstack/usb_storage.c
+++ firmware/usbstack/usb_storage.c
@@ -1476,6 +1476,7 @@ static void fill_inquiry(IF_MD_NONVOID(int lun))
 
 struct usb_class_driver usb_cdrv_storage = {
     .needs_exclusive_storage = true,
+    .needs_cpu_boost = true,
     .config = 1,
     .ep_allocs_size = ARRAYLEN(ep_allocs),
     .ep_allocs = ep_allocs,
